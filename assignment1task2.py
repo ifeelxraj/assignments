d2081ea879a5d0f877617f56ca327a930606131e
ifeelxraj <upmanyuraj5@gmail.com>
2025-10-19 23:30:27 +0530

    assignments

---
+++ assignment1task2.py
@@ -0,0 +1,5 @@
+firstname = input("Enter your first name : ")
+lastname = input("Enter your last name : ")
+
+fullname = f"{firstname} {lastname}"
+print(f"Your full name is {fullname} , Welcome to Python Programming")
